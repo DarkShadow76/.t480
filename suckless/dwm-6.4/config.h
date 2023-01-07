6dd18ec0e522f48c6f05d12f13cb1d93d1ed4d7a
AlonsoCGonzales <alonzotoricaceres@gmail.com>
2023-01-07 09:01:36 -0500

    dwm

--- suckless/dwm-6.4/config.h
+++ suckless/dwm-6.4/config.h
@@ -10,7 +10,7 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#dddddd";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#993399";
 static const char *colors[][3]      = {
