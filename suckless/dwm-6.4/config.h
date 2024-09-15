06ad816335a3ee463295f81d8fd28d615fd960cb
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-09-15 17:57:26 -0500

    bottom Stack

--- suckless/dwm-6.4/config.h
+++ suckless/dwm-6.4/config.h
@@ -53,6 +53,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "TTT",      bstack },    /* bottomstack layout */
+	{ "===",      bstackhoriz }, /* horizontal bottomstack layout */
 };
 
 /* key definitions */
@@ -91,6 +93,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_v,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
